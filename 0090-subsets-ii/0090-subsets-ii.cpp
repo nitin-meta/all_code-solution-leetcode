f7d59cb07c8386c5f92c9633ca81d25c6f2f38e5
NITIN MANOJ <93020908+nitin-meta@users.noreply.github.com>
2023-05-02 17:41:35 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (87.98%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void findsub(int ind,vector<int>&nums,vector<int>&ds,vector<vector<int>>&ans){
+        ans.push_back(ds);
+        for(int i=ind;i<nums.size();i++){
+            if(i!=ind && nums[i]==nums[i-1]) continue;
+            ds.push_back(nums[i]);
+            findsub(i+1,nums,ds,ans);
+            ds.pop_back();
+        }
+    }
+    public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        sort(nums.begin(),nums.end());
+        findsub(0,nums,ds,ans);
+        return ans;
+    }
+};
