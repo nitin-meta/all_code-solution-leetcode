7b22025bc8a0a874c08ec0222d24d313e992f9c9
NITIN MANOJ <93020908+nitin-meta@users.noreply.github.com>
2024-01-29 12:03:46 +0530

    Time: 115 ms (95.88%), Space: 103.3 MB (65.84%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,23 +1,30 @@
+//Using BFS
 class Solution {
 public:
     int n;
-    void dfs(vector<int>&vis,vector<int>&d,int node,vector<int>& edges){
-        vis[node]=1;
-        int v=edges[node];
-        if(v!=-1 && !vis[v]){
-            vis[v]=1;
-            d[v]=1+d[node];
-            dfs(vis,d,v,edges);
-        }        
+    void bfs(vector<int>&d,int node,vector<int>& edges){
+        queue<int>q;
+        d[node]=0;
+        vector<bool>vis(n,false);
+        q.push(node);
+        vis[node]=true;
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            int v=edges[u];
+            if(v!=-1 && !vis[v]){
+                vis[v]=true;
+                d[v]=1+d[u];
+                q.push(v);
+            }
+        }
     }
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         //Takeway: Distance from one node to another and stored in vector
-        n=edges.size();
+         n=edges.size();
         vector<int>d1(n,1e9),d2(n,1e9);//Distance Vector
-        d1[node1]=0,d2[node2]=0;//Khud se khud ka duri 0 hoga na
-        vector<int>vis1(n,0),vis2(n,0);
-        dfs(vis1,d1,node1,edges);
-        dfs(vis2,d2,node2,edges);
+        bfs(d1,node1,edges);
+        bfs(d2,node2,edges);
         int res=-1,minD=1e9;
         for(int i=0;i<n;i++){
             int maxi=max(d1[i],d2[i]);
