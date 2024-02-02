3bf3a837ace9309d6cbb361cc4a8e75a8796e095
NITIN MANOJ <93020908+nitin-meta@users.noreply.github.com>
2024-02-02 10:52:46 +0530

    Time: 73 ms (34.53%), Space: 47.5 MB (32.78%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,50 +1,56 @@
-class Node {
+class Node{
 public:
     char data;
-    unordered_map<char, Node*> m;
+    unordered_map<char,Node*>mp;
     bool isTerminal;
-    
-    Node(char d) {
-        data = d;
-        isTerminal = false;
+    Node(char d){
+        data=d;
+        isTerminal=false;
     }
 };
 
 class Trie {
 public:
     Node* root;
-
     Trie() {
-        root = new Node('\0');
+        root=new Node('\0');
     }
-
+    
     void insert(string word) {
-        Node* temp = root;
-        for (auto ch : word) {
-            if (temp->m.count(ch) == 0) {
-                Node* n = new Node(ch);
-                temp->m[ch] = n;
+        Node* temp=root;
+        for(auto ch:word){
+            if(temp->mp.count(ch)==0){
+                Node *n= new Node(ch);
+                temp->mp[ch]=n;
             }
-            temp = temp->m[ch];
+            temp=temp->mp[ch];
         }
-        temp->isTerminal = true;
+        temp->isTerminal=true;
     }
-
+    
     bool search(string word) {
-        Node* temp = root;
-        for (auto ch : word) {
-            if (temp->m.count(ch) == 0) return false;
-            temp = temp->m[ch];
+        Node* temp=root;
+        for(auto ch:word){
+            if(temp->mp.count(ch)==0) return false;
+            temp=temp->mp[ch];
         }
         return temp->isTerminal;
     }
-
+    
     bool startsWith(string prefix) {
-        Node* temp = root;
-        for (auto ch : prefix) {
-            if (temp->m.count(ch) == 0) return false;
-            temp = temp->m[ch];
+        Node* temp=root;
+        for(auto ch:prefix){
+            if(temp->mp.count(ch)==0) return false;
+            temp=temp->mp[ch];
         }
         return true;
     }
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
